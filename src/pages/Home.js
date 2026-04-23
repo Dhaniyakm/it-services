bd96e37db31797aa42620e6236f7f110cdccc8a0
Dhaniya <dhaniyakm2005@gmail.com>
2026-04-23 14:12:00 +0530

    navbar update 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { TbBackground } from "react-icons/tb";
 
 function Home() {
   return (
-    <div>
-      <h1><center>Welcome to Infynix Tech</center></h1>
-      <p><h3><center>We provide web development, app development and IT solutions.</center></h3></p>
-      <ul>
-        <li><i>Empowering businesses with innovative IT solutions</i></li></ul>
-        <ul><li><i>Transforming ideas into digital reality</i></li></ul>
-        <ul><li><i>Building smart, scalable, and secure applications</i></li></ul>
-        <ul><li><i>Your trusted partner in digital transformation</i></li></ul>
-        <ul><li><i>Delivering technology that drives success</i></li></ul>
-      
+    <div className="home-container">
+      <h1 style={{ textAlign: "center" }}>Welcome to Infynix Tech</h1>
+
+      <h3 style={{ textAlign: "center", marginTop: "10px" }}>
+        We provide web development, app development and IT solutions.
+      </h3>
+
+      <ul style={{ marginTop: "30px", lineHeight: "2", fontSize: "18px" }}>
+        <li><i>Empowering businesses with innovative IT solutions</i></li>
+        <li><i>Transforming ideas into digital reality</i></li>
+        <li><i>Building smart, scalable, and secure applications</i></li>
+        <li><i>Your trusted partner in digital transformation</i></li>
+        <li><i>Delivering technology that drives success</i></li>
+      </ul>
     </div>
   );
 }
